4de25efb9e3fed92070ebcf12c6d85c41ef07d69
Erik Guillén <erik.ur.gf.10@gmail.com>
2023-05-22 11:44:29 -0500

    test 3

--- datos/configuracion.php
+++ datos/configuracion.php
@@ -5,7 +5,7 @@ define("TIPO_BD","mysql"); //PG MYSQL
 define("SERVIDOR_BD", "localhost");
 define("USUARIO_BD", "root");
 //define("CLAVE_BD", "X)r4M_zH3CNk/udf");
-define("CLAVE_BD", "");
+define("CLAVE_BD", "x");
 define("PUERTO_BD", "3306");
 define("NOMBRE_BD", "bd_dpi");
 //1033, COIGO DE YA REGISTRAD
