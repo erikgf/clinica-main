9ea753f86eaa226de8cbfa22a5992bbc66467873
Erik Guillén <erik.ur.gf.10@gmail.com>
2023-05-22 11:43:13 -0500

    test 2

--- datos/configuracion.php
+++ datos/configuracion.php
@@ -4,6 +4,7 @@ date_default_timezone_set('America/Lima');
 define("TIPO_BD","mysql"); //PG MYSQL
 define("SERVIDOR_BD", "localhost");
 define("USUARIO_BD", "root");
+//define("CLAVE_BD", "X)r4M_zH3CNk/udf");
 define("CLAVE_BD", "");
 define("PUERTO_BD", "3306");
 define("NOMBRE_BD", "bd_dpi");
