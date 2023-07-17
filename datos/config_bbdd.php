73a687f330ac3cdd099258b12cf7d24775faa922
erikgf <erik.ur.gf.10@gmail.com>
2023-07-17 14:10:32 -0500

    reajuste configuraciones

--- datos/config_bbdd.php
+++ datos/config_bbdd.php
@@ -3,7 +3,7 @@ date_default_timezone_set('America/Lima');
 
 define("TIPO_BD","mysql"); //PG MYSQL
 define("SERVIDOR_BD", "localhost");
-define("USUARIO_BD", "root");
+define("USUARIO_BD", "X)r4M_zH3CNk/udf");
 define("CLAVE_BD", "");
 define("PUERTO_BD", "3306");
-define("NOMBRE_BD", "bd_dpi_neo");
+define("NOMBRE_BD", "bd_dpi");
--- datos/configuracion.php
+++ datos/configuracion.php
@@ -4,8 +4,7 @@ date_default_timezone_set('America/Lima');
 define("TIPO_BD","mysql"); //PG MYSQL
 define("SERVIDOR_BD", "localhost");
 define("USUARIO_BD", "root");
-//define("CLAVE_BD", "X)r4M_zH3CNk/udf");
-define("CLAVE_BD", "");
+define("CLAVE_BD", "X)r4M_zH3CNk/udf");
 define("PUERTO_BD", "3306");
 define("NOMBRE_BD", "bd_dpi");
 //1033, COIGO DE YA REGISTRAD
