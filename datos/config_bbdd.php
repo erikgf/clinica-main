b9c3ad062d0aef62204ebc174743d842a9d0d477
Erik Guillén <erik.ur.gf.10@gmail.com>
2023-08-25 05:37:44 -0500

    update completo cached

--- datos/config_bbdd.php
+++
@@ -1,9 +0,0 @@
-<?php 
-date_default_timezone_set('America/Lima');
-
-define("TIPO_BD","mysql"); //PG MYSQL
-define("SERVIDOR_BD", "localhost");
-define("USUARIO_BD", "");
-define("CLAVE_BD", "");
-define("PUERTO_BD", "3306");
-define("NOMBRE_BD", "bd_dpi");
